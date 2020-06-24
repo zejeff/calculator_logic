d61aae55b047970c6c1e1cdfeedf6f1f2a48c9c5
Jeffry <jeffryfleischer@gmail.com>
2020-06-23 19:48:15 -0700

    add index.html page code with script call, add script for add logic

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+alert(result);

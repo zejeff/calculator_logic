0703614bac69e58901e5402e2f6ea7efe44208b4
Jeffry <jeffryfleischer@gmail.com>
2020-07-07 17:51:21 -0700

    add complete operation for add()

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
+//Business logic
 function add(number1, number2) {
   return number1 + number2;
-}
+};
 
 function subtract(number1, number2) {
   return number1 - number2;
-}
+};
 
 function divide(number1, number2) {
   return number1 / number2;
-}
+};
 
 function multiply(number1, number2) {
   return number1 * number2;
-}
+};
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
-const addResult = add(number1, number2);
-const subtractResult = subtract(number1, number2);
-const divideResult = divide(number1, number2);
-const multiplyResult = multiply(number1, number2);
-
-alert(divideResult);
+//User Interface
+$(document).ready(function() {
+  $("#calculatorFormAdd").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("input#number1").val());
+    const number2 = parseInt($("input#number2").val());
+    console.log(add(number1, number2))
+  });
+});

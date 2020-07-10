a46ededd83b0851affb3d2de7114561d5d3985fe
Jeffry <jeffryfleischer@gmail.com>
2020-07-09 18:34:35 -0700

    add complete interface logic for add, subtract, multiply and divide

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,24 @@ function multiply(number1, number2) {
 
 //User Interface
 $(document).ready(function() {
-  $("#calculatorFormAdd").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    const number1 = parseInt($("input#number1").val());
-    const number2 = parseInt($("input#number2").val());
-    console.log(add(number1, number2))
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2); 
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+    console.log("1st number: " + number1);    // for debugging
+    console.log("2nd number: " + number2);    // for debugging
+    console.log("operator: " + operator);     // for debugging
   });
 });
